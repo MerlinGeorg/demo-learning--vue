94503f2142a297f82df510033592bd2c9005e876
MerlinGeorg <merlingeo96@gmail.com>
2022-11-11 11:35:25 +0530

    practice codes

---
+++ src/views/AboutView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <label for=""><input type="checkbox" value="vanilla" v-model="iceCreamFlavours">Vanilla</label>
+<label for=""><input type="checkbox" value="vanilla" v-model="iceCreamFlavours">Choclate</label>
+<label for=""><input type="checkbox" value="vanilla" v-model="iceCreamFlavours">Coffee</label>
+  </div>
+</template>
